0a2cb7d8df9208972380f81950f94113dc07566c
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-19 02:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-const readline = require('readline');
-
-// Create readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Function to check if a year is a leap year
-function year(n) {
-  if ((n % 4 === 0 && n % 100 !== 0) || n % 400 === 0) {
-    return 366; // Leap year
-  } else {
-    return 365; // Not a leap year
-  }
-}
-
-// Take input from the user
-rl.question(' ', (input) => {
-const n = parseInt(input); // Convert input to an integer
-const days = year(n); // Call the function
-console.log(`${days}`);
-
-  rl.close(); // Close the readline interface
-});
+const daysOfAYear = (year) => {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return 366; 
+            } else {
+                return 365; 
+            }
+        } else {
+            return 366; 
+        }
+    } else {
+        return 365; 
+    }
+};
